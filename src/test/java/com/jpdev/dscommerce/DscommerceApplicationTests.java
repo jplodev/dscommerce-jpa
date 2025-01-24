d036cee37ca5eddeeb5f16a4bd04909684b515cc
João Paulo <jplopapa.dev@gmail.com>
2025-01-24 11:20:13 -0300

    Project created

---
+++ src/test/java/com/jpdev/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
